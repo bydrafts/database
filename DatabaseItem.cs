2f1c8d16bb74c4fff3066bcb2cb418f3970acc7c
Iago Fonteles <iagofonteles1@gmail.com>
2026-03-28 14:29:02 -0300

    non serialized index

--- DatabaseItem.cs
+++ DatabaseItem.cs
@@ -5,15 +5,15 @@ namespace Drafts.Database
 {
     public abstract class DatabaseItem : ScriptableObject, IDatabaseItemInternal
     {
-        [SerializeField, HideInInspector] protected int index;
         [SerializeField] protected Sprite icon;
         
+        [NonSerialized] protected int _index;
         [NonSerialized] protected string _id;
         [NonSerialized] protected string _displayName;
         [NonSerialized] protected string _description;
 
         public virtual string Id => _id ??= $"{GetType().Name} {name}".ToLower();
-        public virtual int Index => index;
+        public virtual int Index => _index;
         public virtual string DisplayName => _displayName ??= name;
         public virtual string Description => _description ??= Id + "-desc";
         public virtual Sprite Icon => icon;
